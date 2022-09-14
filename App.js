44d68c21916bc600ccbfe9901eaf03d6e1ce0ade
minikastek <97316967+minikastek@users.noreply.github.com>
2022-09-14 20:44:10 -0300

    cambios para desafio
    cambios para que funcione correctamente

--- App.js
+++ App.js
@@ -1,73 +1,163 @@
 import { useState } from 'react';
-import { FlatList, Button,TextInput, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Button,TextInput, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { AddTask, CustomModal  } from './src/components/index';
 
 export default function App() {
 
-  const [ToDo, setToDo] = useState()
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedTask, setSelectedTask] = useState(null);
 
-  const [ToDos, setToDos] = useState([{key: 'nico'}, {key: 'Luis'}, {key: 'Alberto'}])
+  const onHandleChangeText = (text) => {
+    console.warn('text', text);
+    setTask(text);
+  }
 
+  const addItem = () => {
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      { id: Date.now(), value: task },
+    ]);
+    setTask('');
+  }
 
-  const addNewTodo = () => {
-    setToDos(ToDo, ...ToDos)
+  const onHandleModal = (id) => {
+    setModalVisible(!modalVisible);
+    setSelectedTask(tasks.find((item) => item.id === id))
+  }
+
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Text style={styles.item}>{item.value}</Text>
+      <TouchableOpacity style={styles.button} onPress={() => onHandleModal(item.id)}>
+        <Text style={styles.delete}>X</Text>
+      </TouchableOpacity>
+    </View>
+  )
+
+  const onHandleDeleteItem = (id) => {
+    setTasks(tasks.filter((item) => item.id !== id));
+    setSelectedTask(null);
+    setModalVisible(!modalVisible);
   }
 
   return (
     <View style={styles.container}>
-
-      <View style={styles.inputContainer}>
-        <TextInput  
-          placeholder='New Task' 
-          style={styles.input}
-          onChange={newTodo => setToDo(newTodo)}
-        />
-        <Button title='add' style={styles.buttonAdd} onPress={() => addNewTodo} color='#adc178'/> 
-      </View>
-
-      <View>
-      <FlatList
-        data={ToDos}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      <AddTask 
+        item={task}
+        onChangeText={onHandleChangeText}
+        placeholder='new task'
+        addItem={addItem}
+        selectionColor='#4A306D'
+        placeholderTextColor='#4A306D'
+        textButton='ADD'
+        color='#4A306D'
       />
-      </View>
-
+      <FlatList
+        style={styles.itemList}
+        data={tasks}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        showsVerticalScrollIndicator={false}
+      />
+      <CustomModal animationType='slide' visible={modalVisible}>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Detalle de la lista</Text>
+        </View>
+        <View style={styles.modalMessageContainer}> 
+          <Text style={styles.modalMessage}>¿Estas seguro de que quieres eliminar?</Text>
+        </View>
+        <View style={styles.modalMessageContainer}> 
+          <Text style={styles.selectedTask}>{selectedTask?.value}</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button 
+            title='Eliminar'
+            onPress={() => onHandleDeleteItem(selectedTask?.id)}
+            color='#4A306D'
+          />
+          <Button 
+            title='Cancelar'
+            onPress={() => setModalVisible(!modalVisible)}
+            color='#cccccc'
+          />
+        </View>
+      </CustomModal>
     </View>
-    
   );
 }
 
-const styles = StyleSheet.create({
 
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    
+    backgroundColor: '#ffffff',
   },
-
-  inputContainer:{
-    marginTop: 50,
-    marginBottom: 20,
+  itemList: {
+    flex: 1,
+    marginVertical: 20,
     marginHorizontal: 20,
+  },
+  itemContainer: {
+    flex: 1,
+    marginVertical: 5,
+    justifyContent: 'space-between',
     flexDirection: 'row',
-    justifyContent:'space-between'
+    alignItems: 'center',
+    marginVertical: 10,
+    backgroundColor: '#9F84BD',
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
-
-  input: {
-    borderBottomColor: '#BCEBCB',
-    borderBottomWidth: 1,
-    height: 45,
-    width: '75%'
-  },
-
-  buttonAdd: {
-    marginTop: '10%'
-  },
-
   item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-    color: '#8f069c'
+    fontSize: 16,
+    color: '#000000',
   },
-
+  delete: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#ffffff'
+  },
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+    paddingVertical: 20,
+  },
+  modalTitle: {
+    fontSize: 16
+  },
+  modalMessageContainer : {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  modalMessage: {
+    fontSize: 14,
+  },
+  selectedTask: {
+    fontSize: 16,
+    color: '#000000',
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginHorizontal: 20,
+  },
+  button: {
+    backgroundColor: '#4A306D',
+    padding: 10,
+    borderRadius: 10,
+  }
 });
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as CustomModal } from './modal/index';
+export { default as AddTask } from './addTask/index';
